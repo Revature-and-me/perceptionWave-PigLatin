f43b925b187c35347c3908fbc8bc7f4d7d760da1
Jericho Nasser <jerichonasser@yahoo.com>
2023-10-23 19:19:26 +0000

    JN

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -17,6 +17,10 @@ public class PigLatin {
      * @return the pig latin form of in.
      */
     public String returnPigLatin(String in){
-        return "";
+        String firstLetter = in.substring(0, 1);
+        String sub = in.substring(1);
+        sub = sub.concat(firstLetter + "ay");
+
+        return sub;
     }
 }
